2817209794479a81a9ecafa6a6c718e588ae7bf2
mukhid10 <mukhid976@gmail.com>
2022-02-10 13:59:27 -0800

    resep favorit validasi

--- halamanAwal/script.js
+++ halamanAwal/script.js
@@ -4,3 +4,15 @@ function deletLogin(){
     localStorage.removeItem('login');
     location.replace('../login/index.html')
 }
+
+let getLogin = JSON.parse(localStorage.getItem("login"))
+let sateAyam = document.querySelector('#sateAyam');
+sateAyam.onclick = sate;
+function sate (){
+    if(getLogin){
+        location.replace('../resepFavorite/sateAyam/index.html')
+    }else{
+        alert('Login dulu');
+        location.replace('../login/index.html')
+    }
+}
