4fe2de9c33f8bcb1ae2dc695af8077ca6d8e4c4c
mukhid10 <mukhid976@gmail.com>
2022-02-11 05:48:03 -0800

    nav done, fitur nav sudah ada fungsi dll

--- halamanAwal/script.js
+++ halamanAwal/script.js
@@ -6,13 +6,50 @@ function deletLogin(){
 }
 
 let getLogin = JSON.parse(localStorage.getItem("login"))
-let sateAyam = document.querySelector('#sateAyam');
+let sateAyam = document.querySelector('.sateAyam');
 sateAyam.onclick = sate;
 function sate (){
     if(getLogin){
         location.replace('../resepFavorite/sateAyam/index.html')
     }else{
-        alert('Login dulu');
-        location.replace('../login/index.html')
+        let goLogin = confirm('Login Dulu Guys')
+        if(goLogin){
+            location.replace('../login/index.html')
+        }   
     }
+}
+
+let resep = document.querySelector('#resep')
+let loginRegister = document.querySelector('#register')
+let dropMenu = document.querySelector('#dropMenu')
+
+if(getLogin){
+    loginRegister.style.display = 'none'
+}else{
+    dropMenu.style.display = 'none'
+}
+
+let indonesia = document.querySelector('#ind')
+let western = document.querySelector('#west')
+
+function indFood(){
+    if(getLogin){
+        location.replace('../resepFavorite/sateAyam/index.html')//jgn lupa di ubah ke list resep
+    }else{
+        let goLogin = confirm('Login Dulu Guys')
+        if(goLogin){
+            location.replace('../login/index.html')
+        }   
+    }
+}
+
+function westertFood(){
+    if(getLogin){
+        location.replace('../resepFavorite/sateAyam/index.html')//jgn lupa di ubah ke list resep
+    }else{
+        let goLogin = confirm('Login Dulu Guys')
+        if(goLogin){
+            location.replace('../login/index.html')
+        }   
+    } 
 }
